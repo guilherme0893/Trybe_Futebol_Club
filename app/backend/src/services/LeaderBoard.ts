d0bf5abbc921852349da8573f6091fa8bceb3b30
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-09 20:33:31 -0300

    creates fillTeamStas & createLeaderBoard functions

--- app/backend/src/services/LeaderBoard.ts
+++ app/backend/src/services/LeaderBoard.ts
@@ -1,9 +1,11 @@
 import IMatch from '../interfaces/Match';
 import ITeam from '../interfaces/Team';
 import Team from './Team';
+import Match from './Match';
 
 class LeaderBoard {
   private teamService = new Team();
+  private matchService = new Match();
   private name: string;
   private totalPoints = 0;
   private totalGames = 0;
@@ -38,6 +40,32 @@ class LeaderBoard {
     this.goalsBalance = this.goalsFavor - this.goalsOwn;
     this.totalPoints = Number(this.totalVictories * 3) + Number(this.totalDraws);
     this.efficiency = Number((this.totalPoints / ((this.totalGames) * 100)).toFixed(2));
+  }
+
+  public async fillTeamStats() {
+    const teams = await this.teamService.getAllTeams(); // array of teams
+    const matches = await this.matchService.getAllMatches(); // array of matches
+    const correlateTeamToMatch = teams.map((team) => { // MAPEIA team PARA CADA match ---> createTeamStats(team, match)
+      matches.forEach((match) => { this.createTeamStats(team, match); });
+      const correlation = { name: team.teamName,
+        totalGames: this.totalGames,
+        totalPoints: this.totalPoints,
+        totalVictories: this.totalVictories,
+        totalDraws: this.totalDraws,
+        totalLosses: this.totalLosses,
+        goalsFavor: this.goalsFavor,
+        goalsOwn: this.goalsOwn,
+        goalsBalance: this.goalsBalance,
+        efficiency: this.efficiency,
+      };
+      return correlation;
+    });
+    return correlateTeamToMatch;
+  }
+
+  public async createLeaderBoard() {
+    const winner = await this.fillTeamStats();
+    return winner;
   }
 }
 
