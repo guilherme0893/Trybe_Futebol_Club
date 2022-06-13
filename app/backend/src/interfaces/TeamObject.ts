0941026663474a0689bde1df6b98880f11893c0b
Guilherme0893 <guilhermeduarte008@gmail.com>
2022-06-13 17:19:19 -0300

    chore: Create TeamObject for LeaderBoard refact

---
+++ app/backend/src/interfaces/TeamObject.ts
@@ -0,0 +1,14 @@
+class TeamObject {
+  name: string;
+  totalPoints: number;
+  totalGames: number;
+  totalVictories: number;
+  totalLosses: number;
+  totalDraws: number;
+  goalsFavor: number;
+  goalsOwn: number;
+  goalsBalance: number;
+  efficiency: number;
+}
+
+export default TeamObject;
